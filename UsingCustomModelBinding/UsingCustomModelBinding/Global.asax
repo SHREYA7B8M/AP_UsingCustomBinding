3c105cbd5dcaa3063c8c39b7c34086ca67e4243e
s66829020@gmail.com <s66829020@gmail.com>
2023-09-02 23:27:08 +0530

    first commit

---
+++ UsingCustomModelBinding/UsingCustomModelBinding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UsingCustomModelBinding.MvcApplication" Language="C#" %>
---
+++ UsingCustomModelBinding/UsingCustomModelBinding/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsingCustomModelBinding.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+
+    }
+}
